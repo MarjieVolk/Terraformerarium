d8d04df824fe9ff2123e5f5640bf499a6a37a498
knexer <knexer@gmail.com>
2017-04-22 11:37:18 -0700

    Reorganize env and organism datatypes a bit

---
+++ Assets/Scripts/EcosystemComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcosystemComponent : MonoBehaviour, IEcosystem
+{
+    // TODO these are all properties inferred from the scene graph
+    public HashSet<OrganismComponent> ContainedOrganisms { get; }
+    public Multiset<Resource> ProducedResources { get; }
+    public Multiset<Resource> ConsumedResources { get; }
+    public Multiset<Resource> RequiredResources { get; }
+    public int Humidity { get; }
+    public int SoilRichness { get; }
+    public int Temperature { get; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WebServerSharedDeps/IOrganism.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public interface IOrganism
+{
+    Multiset<Resource> ProducedResources { get; }
+    Multiset<Resource> ConsumedResources { get; }
+    Multiset<Resource> RequiredResources { get; }
+
+    int MinimumHumidity { get; }
+    int MaximumHumidity { get; }
+
+    int MinimumSoilRichness { get; }
+    int MaximumSoilRichness { get; }
+
+    int MinimumTemperature { get; }
+    int MaximumTemperature { get; }
+}
+
