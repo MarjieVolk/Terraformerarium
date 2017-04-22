2b2c5698ef335434d8bcb05f078c32280cce2711
Marjie <marjie.volk@gmail.com>
2017-04-22 14:47:48 -0700

    V1 level canvas!  With resizing to fit screen!

---
+++ Assets/Scripts/UI/ScreenResolutionEnforcer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class ScreenResolutionEnforcer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RectTransform content = this.transform.GetChild(0) as RectTransform;
+        float widthRatio = content.rect.width / Screen.width;
+        float heightRatio = content.rect.height / Screen.height;
+        this.GetComponent<CanvasScaler>().scaleFactor = 1 / Mathf.Max(heightRatio, widthRatio);
+	}
+}
