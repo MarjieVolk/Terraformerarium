dbbfea6f8baf1adbfc7047da504944636b51b866
knexer <knexer@gmail.com>
2017-04-22 17:24:28 -0700

    Simple organism library

---
+++ Assets/Scripts/OrganismLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class OrganismLibrary
+{
+    private static Dictionary<string, Organism> Library;
+
+    public static Organism GetOrganismFor(string canonicalName)
+    {
+        Organism org;
+        Library.TryGetValue(canonicalName, out org);
+
+        return org;
+    }
+
+    public static void RegisterOrganism(string canonicalName, Organism org)
+    {
+        Library[canonicalName] = org;
+    }
+}
