617592b93733f8cc074bfd730a4acdfe49313218
David Lurton <dlurton@gmail.com>
2017-04-22 22:11:35 -0700

    Initial commit of server-side WebAPI components.

---
+++ WebAPI/Terraformerarium.Data/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Terraformerarium.Data
+{
+    public class Solution
+    {
+        public int Id { get; set; }
+
+        public int LevelId { get; set; }
+        public Level Level { get; set; }
+
+        public int Score { get; set; }
+
+        public string SolutionData { get; set; }
+    }
+}
