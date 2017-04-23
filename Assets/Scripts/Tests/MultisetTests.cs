14245f34c70373cb40dd177909ac8b3e2d7221eb
Andrew Arbogast <andrewar@microsoft.com>
2017-04-22 17:01:09 -0700

    sample test

---
+++ Assets/Scripts/Tests/MultisetTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal sealed class MultisetTests
+    {
+        [Test]
+        public void TestMultiset()
+        {
+            var m = new Multiset<string>(new []
+            {
+                "a",
+                "a"
+            });
+            Assert.AreEqual(true, m.Contains("a"));
+        }
+    }
+}
