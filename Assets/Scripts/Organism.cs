bd3f7a4aef949dca4c37d4034918c2bb97b84de0
knexer <knexer@gmail.com>
2017-04-22 17:24:14 -0700

    Basic data-only organism

---
+++ Assets/Scripts/Organism.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Organism : IOrganism
+{
+    public Multiset<Resource> ConsumedResources { get; set; }
+
+    public Multiset<Resource> ProducedResources { get; set; }
+
+    public Multiset<Resource> RequiredResources { get; set; }
+
+    public int MinimumHumidity { get; set; }
+
+    public int MaximumHumidity { get; set; }
+
+    public int HumidityMod { get; set; }
+
+    public int MinimumSoilRichness { get; set; }
+
+    public int MaximumSoilRichness { get; set; }
+
+    public int SoilRichnessMod { get; set; }
+
+    public int MinimumTemperature { get; set; }
+
+    public int MaximumTemperature { get; set; }
+
+    public int TemperatureMod { get; set; }
+}
