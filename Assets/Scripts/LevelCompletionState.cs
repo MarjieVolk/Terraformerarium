a8c0a3f02f2b167bb72c0cfffd59a6d59e3c421f
Marjie <marjie.volk@gmail.com>
2017-04-24 19:27:33 -0700

    Menu and level select scenes done!

---
+++ Assets/Scripts/LevelCompletionState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class LevelCompletionState
+{
+    private const string NumCompletedKey = "NumLevelsCompleted";
+
+    public static int NumberOfLevelsCompleted()
+    {
+        if (!PlayerPrefs.HasKey(NumCompletedKey))
+            PlayerPrefs.SetInt(NumCompletedKey, 0);
+
+        return PlayerPrefs.GetInt(NumCompletedKey);
+    }
+
+    public static void CompleteLevel()
+    {
+        PlayerPrefs.SetInt(NumCompletedKey, NumberOfLevelsCompleted() + 1);
+    }
+}
--- Assets/Scripts/SceneFlow/SelectLevelButton.cs
+++ Assets/Scripts/SceneFlow/SelectLevelButton.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
-public class SelectLevelButton : MonoBehaviour {
-
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(PlaySfx))]
+public class SelectLevelButton : MonoBehaviour
+{
     public string level;
+    public int completedLevelsRequired;
 
 	// Use this for initialization
 	void Start () {
-        this.GetComponent<Button>().onClick.AddListener(() => SceneHelper.Obj.GoToLevel(level));
+        if (LevelCompletionState.NumberOfLevelsCompleted() >= completedLevelsRequired)
+            this.GetComponent<Button>().onClick.AddListener(() => SceneHelper.Obj.GoToLevel(level));
+        else
+        {
+            this.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            this.GetComponent<Button>().interactable = false;
+            this.GetComponent<PlaySfx>().enabled = false;
+        }
 	}
 }
